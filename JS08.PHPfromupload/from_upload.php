7a21d0ff53bc11d62d5c3891fbecaf01d6557ab9
ArdhioFatraRevangga <fatraoppo25@gmail.com>
2024-10-15 09:13:38 +0700

    JS08 praktikum 1

---
+++ JS08.PHPfromupload/from_upload.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+</head>
+<body>
+    <title>File Upload</title>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="myfile">
+    <input type="submit" name="submit">
+    </form>
+</body>
+</html>
+
